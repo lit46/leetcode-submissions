14f177a2ff98e63b38a646bb11ed50b407d468a1
S0rBIDNOVARIS <aritrdubey@gmail.com>
2026-06-06 10:48:47 +0530

    Add: binary-search - submission-2

---
+++ Data Structures & Algorithms/binary-search/submission-2.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+
+    int binary(int l, int r, vector<int>& nums, int target){
+        if(l>r) return -1;
+        int mid = (l+r)/2;
+        if(nums[mid]<target) return binary(mid+1,r,nums,target);
+        else if(nums[mid]>target) return binary(l,mid-1,nums,target);
+        else if(nums[mid]==target) return mid;
+        return 0;
+    }
+
+    int search(vector<int>& nums, int target) {
+        return binary(0,nums.size(),nums,target);
+    }
+};
