42f2942aae06a2e5e9dd9c64876f0e725f6ec119
S0rBIDNOVARIS <aritrdubey@gmail.com>
2026-06-06 18:14:41 +0530

    Add: search-insert-position - submission-8

---
+++ Data Structures & Algorithms/search-insert-position/submission-8.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int searchInsert(vector<int>& nums, int target) {
+        int l=0, r=nums.size();
+        if(nums[0]>=target) return 0;
+        if(nums[r-1]<target) return r;
+        while(l<=r){
+            int mid=l+(r-l)/2;
+            if(nums[mid]<target&&nums[mid+1]>=target){
+                return mid+1;
+            }else if(nums[mid]<target){
+                l=mid+1;
+            }else if(nums[mid]>=target&&nums[mid-1]<target){
+                return mid;                
+            }else if(nums[mid]>target){
+                r=mid-1;
+            }
+        }
+        return 0;
+    }
+};
