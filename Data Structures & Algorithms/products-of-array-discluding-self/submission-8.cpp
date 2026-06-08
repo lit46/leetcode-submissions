3af605638d6af6f54cddb4e5bd1f73734075ffbb
S0rBIDNOVARIS <aritrdubey@gmail.com>
2026-06-08 18:24:36 +0530

    Add: products-of-array-discluding-self - submission-8

---
+++ Data Structures & Algorithms/products-of-array-discluding-self/submission-8.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    vector<int> productExceptSelf(vector<int>& nums) {
+        vector<int> output(nums.size(), 1);
+        vector<int> rightprod(nums.size(),1);
+        for(int i=1;i<nums.size();i++){
+            output[i]=output[i-1]*nums[i-1];
+        }
+        for(int i=nums.size()-2;i>-1;i--){
+            rightprod[i] = rightprod[i+1]*nums[i+1];
+        }
+        for(int i=0;i<nums.size();i++){
+            output[i]=output[i]*rightprod[i];
+        }
+        return output;
+    }
+};
