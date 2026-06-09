ef928bd74be132be173f69a75e8e8b38f0236ef8
S0rBIDNOVARIS <aritrdubey@gmail.com>
2026-06-09 15:59:03 +0530

    Add: majority-element-ii - submission-1

---
+++ Data Structures & Algorithms/majority-element-ii/submission-1.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    vector<int> majorityElement(vector<int>& nums) {
+        int n = nums.size();
+        int f = n/3;
+        vector<int> output;
+        unordered_map<int, int> freq;
+        for(int i : nums){
+            freq[i]++;
+        }
+        for(const auto& [key,value]: freq){
+            if(value>f){
+                output.push_back(key);
+            }
+        }
+        return output;
+    }
+};
