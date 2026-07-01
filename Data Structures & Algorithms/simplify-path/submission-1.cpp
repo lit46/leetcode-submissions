2d4b13aa2879c6bf3053bbcd01c2c6469bff6799
S0rBIDNOVARIS <aritrdubey@gmail.com>
2026-07-01 18:26:31 +0530

    Add: simplify-path - submission-1

---
+++ Data Structures & Algorithms/simplify-path/submission-1.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    string simplifyPath(string path) {
+        vector<string> directories;
+        for(int i=0;i<path.size();i++){
+            string cd = "";
+            while(i<path.size()&&path[i]=='/'){
+                i++;
+            }
+            while(i<path.size()&&path[i]!='/'){
+                cd+=path[i];
+                i++;
+            }
+            if(!cd.empty()) directories.push_back(cd);
+        }
+        vector<string> finalpath;
+        for(string dir : directories){
+            if(dir=="."){
+                continue;
+            }else if(dir==".."){
+                if(!finalpath.empty()) finalpath.pop_back();
+            }else{
+                finalpath.push_back(dir);
+            }
+        }
+        string res="";
+        for(string dir: finalpath){
+            res+="/";
+            res+=dir;
+        }
+        if(res.empty()) return "/";
+        return res;
+    }
+};
