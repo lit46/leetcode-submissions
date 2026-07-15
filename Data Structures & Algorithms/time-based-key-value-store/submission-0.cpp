c93545f6c81fbfa5a1230ea2030ea8b38068f44f
S0rBIDNOVARIS <aritrdubey@gmail.com>
2026-07-15 12:41:36 +0530

    Add: time-based-key-value-store - submission-0

---
+++ Data Structures & Algorithms/time-based-key-value-store/submission-0.cpp
@@ -0,0 +1,30 @@
+class TimeMap {
+    unordered_map<string,vector<pair<int,string>>> mp;
+public:
+    TimeMap() {
+        
+    }
+    
+    void set(string key, string value, int timestamp) {
+        mp[key].push_back({timestamp, value});
+    }
+    
+    string get(string key, int timestamp) {
+        if(mp.find(key)==mp.end()){
+            return "";
+        }
+        vector<pair<int, string>>& arr = mp[key];
+        int l=0, r=arr.size()-1;
+        string curr="";
+        while(l<=r){
+            int mid=l+(r-l)/2;
+            if(arr[mid].first<=timestamp){
+                curr=arr[mid].second;
+                l=mid+1;
+            }else{
+                r=mid-1;
+            }
+        }
+        return curr;
+    }
+};
